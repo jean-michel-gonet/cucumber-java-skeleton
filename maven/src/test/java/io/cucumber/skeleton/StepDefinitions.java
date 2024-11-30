7dd487066cfd1006ecc1dcf3e7ca72fd78371823
Jean-Michel Gonet <jmgonet@yahoo.com>
2024-11-30 07:59:08 +0100

    Exception text is never shown

--- maven/src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ maven/src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,11 +1,48 @@
 package io.cucumber.skeleton;
 
+import java.lang.Double;
+import java.util.Map;
+import java.util.List;
+
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.DataTableType;
 
 public class StepDefinitions {
+
+	private static class Ingredient {
+		private String name;
+		private Double quantity; 
+		private String units;
+		
+		private Ingredient(String name, Double quantity, String units) {
+			this.name = name;
+			this.quantity = quantity;
+			this.units = units;
+		}
+	}
+
+    @DataTableType
+    public Ingredient mySystemEntry(Map<String, String> entry) {
+        throw new IllegalArgumentException("This message is never shown during test execution");
+    }
+
     @Given("I have {int} cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) {
         Belly belly = new Belly();
         belly.eat(cukes);
     }
+	
+	@When("I mix the following ingredients")
+	public void i_wait_hour(DataTable dataTable) {
+	    List<Ingredient> ingredients = dataTable.asList(Ingredient.class);
+		
+	}
+
+	@Then("my belly should growl")
+	public void my_belly_should_growl() {
+	    // Write code here that turns the phrase above into concrete actions
+	}
 }
--- maven/src/test/resources/io/cucumber/skeleton/belly.feature
+++ maven/src/test/resources/io/cucumber/skeleton/belly.feature
@@ -2,5 +2,11 @@ Feature: Belly
 
   Scenario: a few cukes
     Given I have 42 cukes in my belly
-    When I wait 1 hour
-    Then my belly should growl
+	
+    When I mix the following ingredients
+	| NAME | QUANTITY | UNITS |
+	| Flour | 1       | KG    |
+	| Water | 0.65    | L     |
+	| Salt  | 0.08    | KG    |
+    
+	Then my belly should growl
